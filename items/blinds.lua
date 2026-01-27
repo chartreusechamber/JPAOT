702dacf88659193efb50e63f2aed199fc28f4cec
chartreusechamber <jacobkennedy006@gmail.com>
2026-01-27 13:49:55 -0500

    i hope you see the blinds

---
+++ items/blinds.lua
@@ -0,0 +1,49 @@
+SMODS.Blind {
+    key = "jpenguin",
+    dollars = 8,
+    mult = 2,
+    boss = { showdown = true },
+    boss_colour = HEX("74CCA8"),
+
+    atlas = "blinds",
+    pos = { x = 0, y = 0 },
+    
+    debuff_hand = function(self, cards, hand, handname, check)
+        if #cards < 1 then return true end
+        local _, _, _, scoring_hand, _ = G.FUNCS.get_poker_hand_info(cards)
+
+        for i, card in ipairs(cards) do
+            -- At least they don't have to be coded again.
+            if not SMODS.in_scoring(card, scoring_hand) and not SMODS.always_scores(card) then return end
+        end
+
+        return true
+    end
+}
+
+SMODS.Blind {
+    key = "jp_mole",
+    dollars = 8,
+    mult = 2,
+    boss = { showdown = true },
+    boss_colour = HEX("A61A1F"),
+
+    atlas = "blinds",
+    pos = { x = 0, y = 0 },
+
+    calculate = function(self, blind, context)
+        if context.nflame_post_shuffle and not blind.disabled then
+            return {
+                sort = function(a, b)
+                    return (a.ability.nflame_scoredcount or 0) > (b.ability.nflame_scoredcount or 0)
+                end,
+                priority = 0,
+                debug = "woawow"
+            }
+        end
+    end,
+
+    disable = function(self)
+        G.deck.cards:shuffle("nflame_mole")
+    end
+}
